213cde22e43d10d8d7d2c8c1e1e7aa2c9151f867
ewraan septian nur R <erwan_ramadhan_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 14:13:09 +0700

    Add files via upload

---
+++ Siswa.java
@@ -0,0 +1,13 @@
+//Object Class
+public class Siswa {
+int id;
+String nama;
+double ipk;
+
+   //Constructor
+   public Siswa() {
+    id = 0;
+    nama = "kosong";
+    ipk = 0.0;
+   }
+}
---
+++ Siswatester.java
@@ -0,0 +1,55 @@
+//Driver Class
+public class Siswatester {
+    public static void main(String[] args) {
+        //object
+        //Class object = new Constructor
+        Siswa Naufal = new Siswa();
+        Siswa Ryuichi =  new Siswa();
+        Siswa Wildan = new Siswa();
+        Siswa Bima = new Siswa();
+        Siswa Destina = new Siswa();
+
+        Naufal.nama = "Naufal";
+        Naufal.id = 28;
+        Naufal.ipk = 97.6;
+
+        System.out.println( "namanya: " + Naufal.nama);
+        System.out.println( "nomornya: " + Naufal.id);
+        System.out.println("nilainya: "+ Naufal.ipk);
+
+        Ryuichi.nama = "Ryuichi";
+        Ryuichi.id = 32;
+        Ryuichi.ipk = 90.0;
+
+        System.out.println( "namanya: " + Ryuichi.nama);
+        System.out.println( "nomornya: " + Ryuichi.id);
+        System.out.println("nilainya: "+ Ryuichi.ipk);
+
+        Wildan.nama = "Wildan";
+        Wildan.id = 23;
+        Wildan.ipk = 98.8;
+
+        System.out.println( "namanya: " + Wildan.nama);
+        System.out.println( "nomornya: " + Wildan.id);
+        System.out.println("nilainya: "+ Wildan.ipk);
+
+        Bima.nama = "Bima"; 
+        Bima.id = 2;
+        Bima.ipk = 95.4;
+
+        System.out.println( "namanya: " + Bima.nama);
+        System.out.println( "nomornya: " + Bima.id);
+        System.out.println("nilainya: "+ Bima.ipk);
+
+        Destina.nama = "destina";
+        Destina.id = 7;
+        Destina.ipk = 98.4;
+
+        System.out.println( "namanya: " + Destina.nama);
+        System.out.println( "nomornya: " + Destina.id);
+        System.out.println("nilainya: "+ Destina.ipk);
+
+
+
+    }
+}
